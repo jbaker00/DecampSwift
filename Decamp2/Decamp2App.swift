42e07beee49d1212331846a6f430154ff80a80a7
James Baker <jamesbake@fb.com>
2022-11-23 12:25:25 -0800

    Initial Commit

---
+++ Decamp2/Decamp2App.swift
@@ -0,0 +1,17 @@
+//
+//  Decamp2App.swift
+//  Decamp2
+//
+//  Created by James Baker on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Decamp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
